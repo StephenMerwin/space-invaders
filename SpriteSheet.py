0d165b6efa3752572cefdce09dd0b2956f769e21
StephenMerwin <32685119+StephenMerwin@users.noreply.github.com>
2021-02-28 19:39:47 -0800

    sprite sheet fixes

--- SpriteSheet.py
+++ SpriteSheet.py
@@ -4,21 +4,21 @@
 # I've added some code to fail if the file wasn't found..
 # Note: When calling images_at the rect is the format:
 # (x, y, x + offset, y + offset)
-​
 import pygame
-​
 
-class spritesheet(object):
+
+# noinspection PyBroadException
+class SpriteSheet(object):
     def __init__(self, filename):
         try:
             self.sheet = pygame.image.load(filename).convert()
-        except:
+        except Exception:
             print('Unable to load spritesheet image:', filename)
             reraise: SystemExit
 
     # Load a specific image from a specific rectangle
     def image_at(self, rectangle, colorkey=None):
-        "Loads image from x,y,x+offset,y+offset"
+        #       "Loads image from x,y,x+offset,y+offset"
         rect = pygame.Rect(rectangle)
         image = pygame.Surface(rect.size).convert()
         image.blit(self.sheet, (0, 0), rect)
@@ -28,18 +28,16 @@ class spritesheet(object):
             image.set_colorkey(colorkey, pygame.RLEACCEL)
         return image
 
-​
 
 # Load a whole bunch of images and return them as a list
 def images_at(self, rects, colorkey=None):
-    "Loads multiple images, supply a list of coordinates"
+    #   "Loads multiple images, supply a list of coordinates"
     return [self.image_at(rect, colorkey) for rect in rects]
 
-​
 
 # Load a whole strip of images
 def load_strip(self, rect, image_count, colorkey=None):
-    "Loads a strip of images and returns them as a list"
+    # "Loads a strip of images and returns them as a list"
     tups = [(rect[0] + rect[2] * x, rect[1], rect[2], rect[3])
             for x in range(image_count)]
     return self.images_at(tups, colorkey)
